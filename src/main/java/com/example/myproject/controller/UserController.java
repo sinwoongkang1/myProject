8b58770248d545ee07cc84c6e45822f13e6b0983
sinwoongkang1 <kkace22@gmail.com>
2024-06-19 18:36:45 +0900

    메인페이지, 회원가입, 로그인 구현

--- src/main/java/com/example/myproject/controller/UserController.java
+++ src/main/java/com/example/myproject/controller/UserController.java
@@ -5,10 +5,7 @@ import com.example.myproject.service.UserService;
 import jakarta.servlet.http.HttpSession;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 
@@ -22,23 +19,33 @@ public class UserController {
     public String eurogIndex() {
         return "eurog";
     }
+
+
     @GetMapping("/join")
     public String join() {
         return "join";
     }
+    //회원가입 진행 - 같은 아이디 존재 확인, 비밀번호 동일하게 입력 확인(JS)
     @PostMapping("/join")
-    public String join(@ModelAttribute User user) {
-        userService.saveUser(user);
-        return "redirect:/eurog";
+    public String join(@ModelAttribute User user, RedirectAttributes redirectAttributes) {
+        if (userService.existsByUsername(user.getUsername())) {
+            redirectAttributes.addFlashAttribute("error", "이미 존재하는 ID입니다. 다른 ID를 사용해주세요.");
+            return "redirect:/eurog/join";
+        } else {
+            userService.saveUser(user);
+            return "redirect:/eurog";
+        }
     }
+
+
     @GetMapping("/login")
     public String login() {
         return "login";
     }
+    //로그인 진행 -  비밀번호 틀릴시 팝업
     @PostMapping("/login")
     public String login(@ModelAttribute User user, HttpSession session, RedirectAttributes redirectAttributes) {
         User foundUser = userService.findUserByUserName(user.getUsername());
-
         if (foundUser != null && user.getPassword().equals(foundUser.getPassword())) {
             session.setAttribute("username", foundUser.getUsername());
             return "redirect:/eurog";
--- src/main/java/com/example/myproject/service/UserService.java
+++ src/main/java/com/example/myproject/service/UserService.java
@@ -5,18 +5,26 @@ import com.example.myproject.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.util.Optional;
+
 @Service
 @RequiredArgsConstructor
 public class UserService {
     private final UserRepository userRepository;
 
-    public User createUser(User user) {
-        return userRepository.save(new User());
+    //가입한 user DB 에 저장하기
+    public void saveUser(User user) {
+        userRepository.save(user);
     }
+
+    //DB에 있는 user 이름으로 찾기
     public User findUserByUserName(String username) {
         return userRepository.findByUsername(username);
     }
-    public void saveUser(User user) {
-         userRepository.save(user);
+
+    //DB에 있는 user 이름과, 입력한 이름 비교해서 boolean 값 반환하기
+    public boolean existsByUsername(String username) {
+        Optional<User> userOptional = Optional.ofNullable(userRepository.findByUsername(username));
+        return userOptional.isPresent();
     }
 }
